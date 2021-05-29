53a6fa4d33e709b1fb9c25731a631a35855165e1
Aryansh <2018eeb1141@iitrpr.ac.in>
2021-05-29 23:52:21 +0530

    first commit:

--- js.js
+++ js.js
@@ -1,3 +1,18 @@
 function clicked(){
     console.log('aryansh')
 }
+
+
+let prevstring = document.querySelector('.aryansh')[0].innerHTML;
+
+
+aryansh.addEventListner('mouseup', function(){
+    document.querySelector('.aryansh')[0].innerHTML = prevstring;
+    console.log("Mouse up");
+})
+
+
+aryansh.addEventListner('mousedown', function(){
+    document.querySelector('.aryansh')[0].innerHTML = "<b>yep...you clicked</b>";
+    console.log("Mouse down")
+})
